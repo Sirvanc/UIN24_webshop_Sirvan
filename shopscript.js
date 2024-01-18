f4e1158dc6c57e6147949a3495b49e5591dc2b47
Sirvan Calabi <88987521+Sirvanc@users.noreply.github.com>
2024-01-18 10:15:03 +0100

    Update shopscript.js

--- shopscript.js
+++ shopscript.js
@@ -14,3 +14,34 @@ console.log(productsHTML)
 
 const main = document.getElementsByTagName("main")
 main[0].innerHTML = productsHTML
+
+function addToCart(productid) {
+    //console.log("addTocart kjører:" + productid);
+    cart.push({ product: productid, quantity: 1 })
+    console.log(cart)
+}
+
+function updateCartDispaly() {
+    let cartHTML = ""
+    if(cart.length === 0) {}
+        cart.map((prod, index) => {
+            let filteredProduct = products.filter(filterprod => prod.product === filterprod.prodid)
+            console.log(filteredProduct)
+    cartHTML += `\<li>
+                        <span class="title">${filteredProduct[0].title}</span>
+                        <span class="price">${filteredProduct[0].price}</span>
+                        <span class="quantity">${Prod.quantity}</span>
+                        <span class="functions">
+                            <button oneclik="remoFromCart(${index})">X</button>
+                        </span>
+                    </li>`;
+            
+        })
+        document.getElementById("cartlist").innerHTML = cartHTML
+        
+}
+function remoFromCart(index) {
+    console.log("Removeing" + index)
+    cart.splice(index, 1)
+    updateCartDispaly()
+}
